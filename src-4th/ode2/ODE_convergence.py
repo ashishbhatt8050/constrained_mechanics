d1449eaf2bae26366b8ab9942f5dc5fbcc0c9173
Ashish Bhatt <ashishbhatt@iitism.ac.in>
2020-04-11 09:56:58 +0530

    reorganize code

--- src-4th/ode2/ODE_convergence.py
+++ src-4th/ode2/ODE_convergence.py
@@ -9,7 +9,7 @@ import ODESolver
 from scitools.std import *
 from numpy import linalg as LA
 
-def test_convergence_and_energy(u_init, u_exact, en_err, T, f, dfdu =None, beta =0, dt_space =None):
+def test_convergence_and_energy(u_init, u_exact, en_err, T, solver):
     """
     Find convergence rate r and coefficient C based on the formula
     error = C dt^r
@@ -20,55 +20,38 @@ def test_convergence_and_energy(u_init, u_exact, en_err, T, f, dfdu =None, beta 
 
     figure()
         
-    registered_solver_classes = [ODESolver.ConformalImplicitMidpoint, ODESolver.ConformalStormerVerlet]
-    
-    for solver_class in registered_solver_classes:
-        legends = []
-        mean_error = []
-        energy_err = lambda u, t: np.asarray(en_err(u, t), float)
-        dt_space = geomspace(0.2, 0.001, num =5)
-    
-        for dt in dt_space:
-            n = int(round(T/dt))
-            if solver_class == registered_solver_classes[0]:
-                solver = solver_class(f[0], dfdu[0], size(u_init), beta)
-                subplot(221)
-            elif solver_class == registered_solver_classes[1]:
-                solver = solver_class(f[1], beta)
-                subplot(223)
-                
-            solver.set_initial_condition(u_init)
-            u, t = solver.solve(linspace(0, T, n+1))
-                
-            plot(t, u[:, 0])
-            legends.append('dt=%g' % dt)
-            hold('on')
+    legends = []
+    mean_error = []
+    energy_err = lambda u, t: np.asarray(en_err(u, t), float)
+    dt_space = geomspace(0.2, 0.001, num =5)
+
+    for dt in dt_space:
+        n = int(round(T/dt))
             
-            mean_error.append(sqrt(dt)*LA.norm(u[:, 0] -u_exact(t)))
+        solver.set_initial_condition(u_init)
+        u, t = solver.solve(linspace(0, T, n+1))
             
-            if solver_class == registered_solver_classes[0]:
-                subplot(222)
-                plot(t, energy_err(u, t), label ='dt=%g' % dt)
-                hold('on')
-                legend()
-                title('%s' % solver_class.__name__)
-            elif solver_class == registered_solver_classes[1]:
-                subplot(224)
-                plot(t, energy_err(u, t), label = 'dt=%g' % dt)
-                hold('on')
-                legend()
-                title('%s' % solver_class.__name__)
+        subplot(121), plot(t, u[:, 0])
+        legends.append('dt=%g' % dt)
+        hold('on')
         
-        subplot(221)
-        plot(t, u_exact(t))
-        legends.append('exact')
-        legend(legends)
-        savefig('tmp_osc.pdf')
+        mean_error.append(sqrt(dt)*LA.norm(u[:, 0] -u_exact(t)))
         
-        # Estimate Convergence rate r and coefficient C
-        r = log(roll(mean_error, -1)[:4]/mean_error[:4])\
-            /log(roll(dt_space, -1)[:4]/dt_space[:4])
-            
-        C = mean_error/dt_space    
-            
-        print solver_class.__name__, r, C
+        subplot(122), plot(t, energy_err(u, t), label = 'dt=%g' % dt)
+        hold('on')
+        legend()
+        title('%s' % type(solver).__name__)
+    
+    subplot(121)
+    plot(t, u_exact(t))
+    legends.append('exact')
+    legend(legends)
+    savefig('tmp_%s.pdf' % type(solver).__name__)
+    
+    # Estimate Convergence rate r and coefficient C
+    r = log(roll(mean_error, -1)[:4]/mean_error[:4])\
+        /log(roll(dt_space, -1)[:4]/dt_space[:4])
+        
+    C = mean_error/dt_space    
+        
+    print type(solver).__name__, r, C
--- src-4th/ode2/app3_osc.py
+++ src-4th/ode2/app3_osc.py
@@ -7,27 +7,42 @@ written as a 2x2 first-order ODE system and solved
 by classes in the ODESolver hierarchy of methods.
 """
 
+import ODESolver
+from scitools.std import *
+#from matplotlib.pyplot import *
+
+registered_solver_classes = [ODESolver.ForwardEuler, ODESolver.ImplicitMidpoint, ODESolver.ConformalImplicitMidpoint, ODESolver.ConformalStormerVerlet]
+
 class OscSystem:
-    def __init__(self, m, beta, k, g, w):
-        self.m, self.beta, self.k, self.g, self.w = \
-                float(m), float(beta), float(k), float(g), w
+    def __init__(self, m, beta, k, g, w, method = None):
+        self.m, self.beta, self.k, self.g, self.w, self.method = \
+                float(m), float(beta), float(k), float(g), w, method
 
     def __call__(self, u, t):
         u0, u1 = u
-        m, beta, k, g, w = \
-           self.m, self.beta, self.k, self.g, self.w
+        m, beta, k, g, w, method = \
+           self.m, self.beta, self.k, self.g, self.w, self.method
         # Use a finite difference for w''(t)
         h = 1E-5
         ddw = (w(t+h) - 2*w(t) + w(t-h))/(h**2)
-        f = [u1, ddw  + g - beta/m*u1 - k/m*u0]
+        if method in ['ForwardEuler', 'BackwardEuler', 'RungeKutta4', 'ImplicitMidpoint']:
+            f = [u1, ddw  + g - beta/m*u1 - k/m*u0]
+        elif method in ['ConformalImplicitMidpoint']:
+            f = list([u1, ddw  + g - beta/m*u1 - k/m*u0] +beta/2*u)
+        elif method in ['ConformalStormerVerlet']:
+            f = [u1, ddw  + g - k/m*u0]
+        else:
+            NameError('f not defined for the given method %s' % method)
         return f
 
 class Jacobian(OscSystem):
     def __call__(self, u, t, dt):
-        m, beta, k, g, w = \
-           self.m, self.beta, self.k, self.g, self.w
-
-        dfdu = [[0.0, 1.0],[-k/m, -beta/m]]
+        if self.method in ['BackwardEuler','ImplicitMidpoint']:
+            dfdu = [[0.0, 1.0],[-k/m, -beta/m]]
+        elif self.method in ['ConformalImplicitMidpoint']:
+            dfdu = [[beta/2, 1.0],[-k/m, beta/2-beta/m]]
+        else:
+            NameError('Jacobian not provided for %s' % self.method)
         return dfdu
     
 class ExactSolution(OscSystem):
@@ -49,52 +64,23 @@ class EnergyError(OscSystem):
            
         #return E(u) -exp(-beta*dt)*E(roll(u,1))
         return abs(E(u) -exp(-beta*t)*E(resize(u[0,:],(1,2))))
-    
-class Getf1(OscSystem):
-    def __call__(self, u, t):
-        u0, u1 = u
-        m, beta, k, g, w = \
-           self.m, self.beta, self.k, self.g, self.w
-        # Use a finite difference for w''(t)
-        h = 1E-5
-        ddw = (w(t+h) - 2*w(t) + w(t-h))/(h**2)
-        f1 = list([u1, ddw  + g - beta/m*u1 - k/m*u0] +beta/2*u)
-        return f1
-    
-class Getdfdu1(OscSystem):
-    def __call__(self, u, t, dt):
-        m, beta, k, g, w = \
-           self.m, self.beta, self.k, self.g, self.w
-
-        dfdu1 = [[beta/2, 1.0],[-k/m, beta/2-beta/m]]
-        return dfdu1
         
 # Test case: u = cos(t)
-import ODESolver
-from scitools.std import *
-#from matplotlib.pyplot import *
-legends = []
 
 m, beta, k, g, w = 1.0, 0.1, 1.0, 0.0, lambda t: 0
-f, dfdu, u_exact, en_err, f1, dfdu1, f2 = OscSystem(m, beta, k, g, w), \
-            Jacobian(m, beta, k, g, w), \
-            ExactSolution(m, beta, k, g, w), \
-            EnergyError(m, beta, k, g, w), \
-            Getf1(m, beta, k, g, w), \
-            Getdfdu1(m, beta, k, g, w), \
-            OscSystem(m, 0.0, k, g, w)
+u_exact, en_err = ExactSolution(m, beta, k, g, w), EnergyError(m, beta, k, g, w)
             
 u_init = [1, 0]    # initial condition
 nperiods = 3.5     # no of oscillation periods
 T = 2*pi*nperiods
-
-registered_solver_classes = [ODESolver.ConformalImplicitMidpoint, ODESolver.ConformalStormerVerlet]
 
 u0_exact = lambda t: u_exact(t)[0]
 u1_exact = lambda t: u_exact(t)[1]
     
 
 #%%
+legends = []
+
 for solver_class in registered_solver_classes:
     if solver_class == ODESolver.RungeKutta4:
         npoints_per_period = 20
@@ -104,40 +90,49 @@ for solver_class in registered_solver_classes:
     n = npoints_per_period*nperiods
     t_points = linspace(0.0, T, n+1)
     
+    alg = lambda solver_class: solver_class.__name__  # (class) name of algorithm
+    
     if solver_class == ODESolver.ConformalImplicitMidpoint:
-        solver = solver_class(f1, dfdu1, size(u_init), beta)
-        solver.set_initial_condition(u_init)
-        u, t = solver.solve(t_points)
+        f, dfdu = OscSystem(m, beta, k, g, w, alg(solver_class)), \
+            Jacobian(m, beta, k, g, w, alg(solver_class))
+        solver = solver_class(f, dfdu, size(u_init), beta)
     elif solver_class == ODESolver.ConformalStormerVerlet:
-        solver = solver_class(f2, beta)
-        solver.set_initial_condition(u_init)
-        u, t = solver.solve(t_points)
-    else:
+        f = OscSystem(m, beta, k, g, w, alg(solver_class))
+        solver = solver_class(f, beta)
+    elif solver_class in [ODESolver.BackwardEuler, ODESolver.ImplicitMidpoint]:
+        f, dfdu = OscSystem(m, beta, k, g, w, alg(solver_class)), \
+            Jacobian(m, beta, k, g, w, alg(solver_class))
         solver = solver_class(f, dfdu, size(u_init))
-        solver.set_initial_condition(u_init)
-        u, t = solver.solve(t_points)
+    else:
+        f = OscSystem(m, beta, k, g, w, alg(solver_class))
+        solver = solver_class(f, beta)
+    
+    solver.set_initial_condition(u_init)
+    u, t = solver.solve(t_points)
 
     # u is an array of [u0,u1] pairs for each time level,
     # get the u0 values from u for plotting
     u0_values = u[:, 0]
     u1_values = u[:, 1]
     figure()
-    alg = solver_class.__name__  # (class) name of algorithm
+    
     plot(t, u0_values, 'r-',
          t, u0_exact(t), 'b-')
     legend(['numerical', 'exact']),
-    title('Oscillating system; position - %s' % alg)
-    savefig('tmp_oscsystem_pos_%s.pdf' % alg)
+    title('Oscillating system; position - %s' % alg(solver_class))
+    savefig('tmp_oscsystem_pos_%s.pdf' % alg(solver_class))
     figure()
     plot(t, u1_values, 'r-',
          t, u1_exact(t), 'b-')
     legend(['numerical', 'exact'])
-    title('Oscillating system; velocity - %s' % alg)
-    savefig('tmp_oscsystem_vel_%s.pdf' % alg)
+    title('Oscillating system; velocity - %s' % alg(solver_class))
+    savefig('tmp_oscsystem_vel_%s.pdf' % alg(solver_class))
+
 show()
 
 #%% Convergence test
 import sys, os
+
 try:
     from ODE_convergence import test_convergence_and_energy
 except ImportError:
@@ -145,4 +140,21 @@ except ImportError:
     Could not import module "test_convergence_and_energy". Place ODE_convergence.py in this directory
     (%s)
     ''' % (os.path.dirname(os.path.abspath(__file__))))
-test_convergence_and_energy(u_init, u0_exact, en_err, T, [f1,f2], [dfdu1], beta)
+    
+for solver_class in registered_solver_classes:
+    if solver_class == ODESolver.ConformalImplicitMidpoint:
+        f, dfdu = OscSystem(m, beta, k, g, w, alg(solver_class)), \
+            Jacobian(m, beta, k, g, w, alg(solver_class))
+        solver = solver_class(f, dfdu, size(u_init), beta)
+    elif solver_class == ODESolver.ConformalStormerVerlet:
+        f = OscSystem(m, beta, k, g, w, alg(solver_class))
+        solver = solver_class(f, beta)
+    elif solver_class in [ODESolver.BackwardEuler, ODESolver.ImplicitMidpoint]:
+        f, dfdu = OscSystem(m, beta, k, g, w, alg(solver_class)), \
+            Jacobian(m, beta, k, g, w, alg(solver_class))
+        solver = solver_class(f, dfdu, size(u_init))
+    else:
+        f = OscSystem(m, beta, k, g, w, alg(solver_class))
+        solver = solver_class(f, beta)
+        
+    test_convergence_and_energy(u_init, u0_exact, en_err, T, solver)

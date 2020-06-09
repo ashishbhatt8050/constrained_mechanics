0e9c7b6ed347e74c091425934c9a5db63acea8b0
Ashish Bhatt <ashishbhatt@iitism.ac.in>
2020-06-09 21:26:18 +0530

    refine plots for publication, add conda py27 env used

--- src-4th/ode2/app3_osc.py
+++ src-4th/ode2/app3_osc.py
@@ -11,21 +11,19 @@ import ODESolver
 from scitools.std import *
 from numpy import linalg as LA
 from pylab import *
-from PlotScript import plot_data
+from PlotScript import plot_data, tex_table
 from scipy.integrate import solve_ivp
 
 # class definitions
 class OscSystem:
-#    def __init__(self, m, beta, k, g, w, method=None, eqn_type='linear'):
     def __init__(self, method=None):
-        self.m, self.beta, self.k, self.g, self.w, self.method, self.eqn_type = \
-                float(m), float(beta), float(k), float(g), w, method, eqn_type
+        self.m, self.beta, self.k, self.g, self.w, self.method, self.eqn_type, self.u_init = \
+                float(m), float(beta), float(k), float(g), w, method, eqn_type, u_init
 
     def __call__(self, u, t):
         u0, u1 = u
-#        m, beta, k, g, w, method, eqn_type = \
-#           self.m, self.beta, self.k, self.g, self.w, self.method, self.eqn_type
         method = self.method
+
         # Use a finite difference for w''(t)
         h = 1E-5
         ddw = (w(t+h) - 2*w(t) + w(t-h))/(h**2)
@@ -76,8 +74,6 @@ class Jacobian(OscSystem):
 
 class ExactSolution(OscSystem):
     def __call__(self, t):
-#        m, beta, k, g, w = \
-#           self.m, self.beta, self.k, self.g, self.w
         z = numpy.lib.scimath.sqrt(beta**2 -4*k*m)/m
 
         if self.eqn_type == 'linear':
@@ -88,8 +84,6 @@ class ExactSolution(OscSystem):
 
 class EnergyError(OscSystem):
     def __call__(self, u, t):
-#        m, beta, k, g, w = \
-#           self.m, self.beta, self.k, self.g, self.w
 
         if self.eqn_type == 'linear':
             def E(u):
@@ -101,14 +95,16 @@ class EnergyError(OscSystem):
             NameError('Energy Error is not defined for the equation type: %s' % self.eqn_type)
 
 # define system parameters and programming variables
-registered_solver_classes = [ODESolver.ConformalImplicitMidpoint, ODESolver.ImplicitMidpoint, type('builtin', (object,),{})]
+registered_solver_classes = [ODESolver.ConformalImplicitMidpoint, ODESolver.ImplicitMidpoint] #, type('builtin', (object,),{})
 
 m, beta, k, g, w, method, eqn_type, plt_res = 1.0, 0.1, 1.0, 0.0, lambda t: 0, None, 'linear', True
+u_init = [1, 0]    # initial condition
+
 u_exact, en_err = ExactSolution(), EnergyError()
 
-u_init = [1, 0]    # initial condition
-nperiods = 3.5     # no of oscillation periods
-T = 2*pi*nperiods
+nperiods = 15     # no of oscillation periods
+# T = 2*pi*nperiods
+T = 80
 
 u0_exact = lambda t: u_exact(t)[0]
 u1_exact = lambda t: u_exact(t)[1]
@@ -168,35 +164,41 @@ except ImportError:
     ''' % (os.path.dirname(os.path.abspath(__file__))))
 
 for solver_class in registered_solver_classes:
-    if solver_class == ODESolver.ConformalImplicitMidpoint:
-        f, dfdu = OscSystem(alg(solver_class)), Jacobian(alg(solver_class))
-        solver = solver_class(f, dfdu, size(u_init), beta)
-    elif solver_class == ODESolver.ConformalStormerVerlet:
-        f = OscSystem(alg(solver_class))
-        solver = solver_class(f, beta)
-    elif solver_class in [ODESolver.BackwardEuler, ODESolver.ImplicitMidpoint]:
-        f, dfdu = OscSystem(alg(solver_class)), Jacobian(alg(solver_class))
-        solver = solver_class(f, dfdu, size(u_init))
-#    else:
-#        f = OscSystem(alg(solver_class))
-#        solver = solver_class(f, beta)
+    f, dfdu = OscSystem(alg(solver_class)), Jacobian(alg(solver_class))
 
-    dt_space, r1, C1 = test_convergence_and_energy(u_init, u0_exact, T, solver, False)
-#    r2 = nan*zeros(r1.size)
-    dt_space, r2, C2 = test_convergence_and_energy(u_init, u0_exact, T, solver, True)
+    if solver_class == ODESolver.ConformalImplicitMidpoint:
+        solver = solver_class(f, dfdu)
+    elif solver_class == ODESolver.ConformalStormerVerlet:
+        solver = solver_class(f)
+    elif solver_class in [ODESolver.BackwardEuler, ODESolver.ImplicitMidpoint]:
+        solver = solver_class(f, dfdu)
+
+    if solver_class == ODESolver.ConformalImplicitMidpoint:
+        dt_space, r1, C1 = test_convergence_and_energy(u_init, u0_exact, T, solver, False)
+        dt_space, r2, C2 = test_convergence_and_energy(u_init, u0_exact, T, solver, True)
+    elif solver_class == ODESolver.ImplicitMidpoint:
+    #    r2 = nan*zeros(r1.size)
+        dt_space, r3, C3 = test_convergence_and_energy(u_init, u0_exact, T, solver, False)
+        dt_space, r4, C4 = test_convergence_and_energy(u_init, u0_exact, T, solver, True)
 
     if alg(solver_class) == 'builtin':
-        f = OscSystem(alg(solver_class))
-        solver = alg(solver_class)
-        dt_space, r1, C1 = test_convergence_and_energy(u_init, u0_exact, T, solver, False, f)
+        dt_space, r1, C1 = test_convergence_and_energy(u_init, u0_exact, T, alg(solver_class), False, f)
         r2 = nan*zeros(r1.size)
 
-    print type(solver).__name__, " \\\\\n".join([" & ".join(map(str,line)) \
-               for line in np.array((dt_space, np.concatenate(([np.float('nan')],r1)), \
-                                     np.concatenate(([np.float('nan')],r2)))).T])
+array2print = np.array((dt_space, np.concatenate(([np.float('nan')],r1)), \
+                                 np.concatenate(([np.float('nan')],r2)), \
+                                np.concatenate(([np.float('nan')],r3)), \
+                                np.concatenate(([np.float('nan')],r4)))).T
+
+tex_table(alg(solver_class), array2print)
+
+#    print type(solver).__name__, " \\\\\n".join([" & ".join(map(str,line)) \
+#               for line in np.array((dt_space, np.concatenate(([np.float('nan')],r1)), \
+#                                     np.concatenate(([np.float('nan')],r2)))).T])
 
 
 #%% Compute conformal symplecticness error
+fig = figure()
 
 for solver_class in registered_solver_classes:
     if solver_class == ODESolver.RungeKutta4:
@@ -210,9 +212,9 @@ for solver_class in registered_solver_classes:
     f, dfdu = OscSystem(alg(solver_class)), Jacobian(alg(solver_class))
 
     if solver_class == ODESolver.ConformalImplicitMidpoint:
-        solver = solver_class(f, dfdu, size(u_init), beta)
+        solver = solver_class(f, dfdu)
     elif solver_class == ODESolver.ImplicitMidpoint:
-        solver = solver_class(f, dfdu, size(u_init), beta)
+        solver = solver_class(f, dfdu)
 
     if alg(solver_class) != 'builtin':
         solver.set_initial_condition(u_init)
@@ -225,20 +227,23 @@ for solver_class in registered_solver_classes:
     if alg(solver_class) == 'builtin':
         fun = lambda t, u: np.asarray(f(u, t), float)
         sol = solve_ivp(fun, [0, T], u_init, method='Radau', t_eval=t_points)
-        u = sol.y.T
-        t = sol.t
-        symp_error = nan*np.zeros(t.size)
+        u, t, symp_error = sol.y.T, sol.t, nan*np.zeros(t.size)
 
     # plot results
     if plt_res:
-        fig = figure()
-        ax1 = fig.add_subplot(121)
-        plot_data(ax1, t, u, True, False)
-        ax1.legend(["position", "momentum"])
-        ax1.set_ylim(-1,1)
+        if solver_class == ODESolver.ImplicitMidpoint:
+            ax = fig.add_subplot(122)
+            ax.set_ylim(0,0.00030)
+        elif solver_class == ODESolver.ConformalImplicitMidpoint:
+            ax = fig.add_subplot(121)
+            # ax.set_ylim(0.0E-15,5.0E-15)
+        # plot_data(ax1, t, u, True, False)
+        # ax1.legend(["$q$", "$p$"])
+        # ax1.set_ylim(-1,1)
+        ax.set_xlabel('time')
 
-        ax2 = fig.add_subplot(122)
-        plot_data(ax2, t, np.array([en_err(u,t), symp_error]).T, True, False)
-        ax2.legend(['$E_Q$','$E_{cs}$'])
+        plot_data(ax, t, np.array([en_err(u,t), symp_error]).T, True, False)
+        fig.legend([r'$\boldmath{E}_I$',r'$\boldmath{E}_{cs}$'], bbox_to_anchor=(0.5,-0.08), loc='lower center',ncol=2,bbox_transform=fig.transFigure)
+        # fig.tight_layout(rect=[0,-0.2,1,1])
 #        ax2.set_ylim(0.0E-15,3.5E-15)
-#        fig.savefig('app3_%s.pdf' % alg(solver_class))
+        fig.savefig('app3_err_inv.pdf', bbox_inches='tight')
